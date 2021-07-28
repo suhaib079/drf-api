03a5b080557c1929e30a3051e04c0f25d6e464c2
suhaib079 <suhaibabdennabi33@gmail.com>
2021-07-28 15:35:10 +0300

    f

--- rides/urls.py
+++ rides/urls.py
@@ -19,4 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/motor/', include('motor.urls')),
+    path('api-auth',include('rest_framework.urls'))
 ]
+
+
+
+
